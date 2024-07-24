53e650b60e39d9a9fc29fa552876e40896b33cff
nice77duck <niceduck77@gmail.com>
2024-07-24 18:36:40 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Encrypted', '/Memory Lane', '/Downloads', '/Memory Lane/Jalan Jalan Nobita', '/Memory Lane/Jalan Jalan Family',],
+  protectedRoutes: ['/Encrypted', '/Downloads', '/Memory Lane/Jalan Jalan Nobita', '/Memory Lane/Jalan Jalan Family',],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:niceduck@hi2.in',
